65812208d8006eacec3a4b984eb0fe4636bd0664
João Ribeiro <joaosoft@gmail.com>
2018-03-20 16:21:56 +0000

    logger

--- service/global.go
+++ service/global.go
@@ -15,18 +15,6 @@ var log = logger.NewLog(
 	"time":    logger.TIME,
 	"service": "go-test"})
 
-func init() {
-	// Log as JSON instead of the default ASCII formatter.
-	logger.SetFormatter(&logger.JSONFormatter{})
-
-	// Output to stdout instead of the default stderr
-	// Can be any io.Writer, see below for File example
-	logger.SetOutput(os.Stdout)
-
-	// Only log the warning severity or above.
-	logger.SetLevel(logger.DebugLevel)
-}
-
 func getDefaultNSQConfig() *NSQConfig {
 	if value, exists := global["nsq"]; exists {
 		return value.(*NSQConfig)
