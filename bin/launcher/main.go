8f3bf07187cdee45d347de10791115046870c369
João Ribeiro <joaosoft@gmail.com>
2018-04-06 19:13:06 +0100

    fix

--- bin/launcher/main.go
+++ bin/launcher/main.go
@@ -5,18 +5,26 @@ import (
 
 	"go-test/service"
 
+	"time"
+
 	logger "github.com/joaosoft/go-log/service"
+	writer "github.com/joaosoft/go-writer/service"
 )
 
 func main() {
 	var log = logger.NewLog(
 		logger.WithLevel(logger.InfoLevel),
-		logger.WithFormatHandler(logger.JsonFormatHandler),
+		logger.WithFormatHandler(writer.JsonFormatHandler),
 		logger.WithWriter(os.Stdout)).WithPrefixes(map[string]interface{}{
 		"level":   logger.LEVEL,
 		"time":    logger.TIME,
 		"service": "go-test"})
 
+	start := time.Now()
 	test := gotest.NewGoTest(gotest.WithLogger(log), gotest.WithPath("./examples"))
+
 	test.Run()
+
+	elapsed := time.Since(start)
+	log.Infof("ELAPSED TIME: %s", elapsed.String())
 }
