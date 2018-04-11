8d0de783b5deee64309a35a7caff60ca3c1074f3
João Ribeiro <joaosoft@gmail.com>
2018-04-11 16:28:12 +0100

    doc

---
+++ bin/launcher/doc.go
@@ -0,0 +1,9 @@
+/*
+GoTest helps to create integration test in a easy way
+
+you just need to define the test structure and run it.
+
+example on https://github.com/joaosoft/go-test/tree/master/bin/launcher
+
+*/
+package main
