01bd3cef7c17010ad70a5092309ab104bdcf204a
João Ribeiro <joaosoft@gmail.com>
2018-04-11 16:38:44 +0100

    doc

--- service/main.go
+++ service/main.go
@@ -1,11 +1,3 @@
-/*
-GoTest helps to create integration test in a easy way
-
-you just need to define the test structure and run it.
-
-example on https://github.com/joaosoft/go-test/tree/master/bin/launcher
-
-*/
 package gotest
 
 import (
